d8c09cd3eaf89afea98687242e2117f373164f01
abdas <abdas1349@gmail.com>
2023-05-15 11:48:25 -0400

    first push

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into user_details (id,birth_date,name)
+values(1001,current_date(),'Ali');
+
+insert into user_details (id,birth_date,name)
+values(1002,current_date(),'Vei');
+
+insert into user_details (id,birth_date,name)
+values(1003,current_date(),'Deli');
+
+insert into user_details (id,birth_date,name)
+values(1004,current_date(),'Tali');
+
+insert into post (id, description,user_id)
+values(2001,'I want to learn Devops',1001);
+
+insert into post (id, description,user_id)
+values(2002,'I want to learn AWS',1002);
+
+insert into post (id, description,user_id)
+values(2003,'I want to Get Devops Certified',1001);
+
+insert into post (id, description,user_id)
+values(2004,'I want to learn MultiCloud',1002);
